c0244a6370e2828de409ce3717a614bf34da5f17
lechinoix <nicolas.ngomai@gmail.com>
2018-07-21 16:45:22 +0200

    Format error message correctly

--- prevent_merge_conflicts.sh
+++ prevent_merge_conflicts.sh
@@ -13,18 +13,25 @@ do
     continue
   fi
 
+  AUTHOR=$(git log -1 --pretty=format:'%an');
+  LAST_DATE_COMMIT=$(git log -1 --format=%cd | date "+%Y-%m-%d %H:%M:%S");
+
   echo ""
-  echo "Running merge test for $branch"
+  echo "Branch name: $branch"
+  echo "Author: $AUTHOR ($LAST_DATE_COMMIT)";
   git checkout $branch &> /dev/null;
   git merge $CURRENT_BRANCH &> /dev/null;
   if [[ $? -eq 0 ]];
   then
-    echo "No conflicts detected!"
+    echo "Status: ✅  No conflicts detected!"
   else
-    AUTHOR=$(git log -1 --pretty=format:'%an');
-    NUMBER_OF_FILES=$(git ls-files -u | wc -l);
-    echo "Conflicts detected with $branch : $NUMBER_OF_FILES files affected";
-    echo "Contact $AUTHOR to discuss whether you should continue your branch or not";
+    NUMBER_OF_FILES=$(git --no-pager diff --name-status --diff-filter=U | wc -l | sed -e 's/^[ \t]*//');
+    PLURAL=""
+    if [ $NUMBER_OF_FILES -gt 1 ];
+    then
+      PLURAL="s"
+    fi
+    echo "Status: 🚨  Conflicts, $NUMBER_OF_FILES file$PLURAL affected";
   fi
   git reset --hard $branch &> /dev/null;
 done
