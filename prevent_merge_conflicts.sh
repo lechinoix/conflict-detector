f4e1fb5e2091faa6cce00a03c823401a05587360
lechinoix <nicolas.ngomai@gmail.com>
2018-07-21 16:08:53 +0200

    Fix script

--- prevent_merge_conflicts.sh
+++ prevent_merge_conflicts.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-set -e
+echo "Starting test branches for merge conflicts..."
+
 git stash &> /dev/null;
 CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
-for branch in $(git branch -a)
+for branch in $(git for-each-ref --format='%(refname:short)' refs/heads/)
 do
+
+  if [ $branch = $CURRENT_BRANCH ];
+  then
+    continue
+  fi
+
+  echo ""
+  echo "Running merge test for $branch"
   git checkout $branch &> /dev/null;
   git merge $CURRENT_BRANCH &> /dev/null;
-  FILE_CONFLICTS=$(git ls-files -u) &> /dev/null;
-  echo "Merge conflicts for $branch"
-  echo $FILE_CONFLICTS;
-  if [[ $FILE_CONFLICTS ]];
+  if [[ $? -eq 0 ]];
   then
-    echo "Conflicts detected with $branch : $(wc -l $FILE_CONFLICTS) files affected";
+    echo "No conflicts detected!"
+  else
+    AUTHOR=$(git log -1 --pretty=format:'%an');
+    NUMBER_OF_FILES=$(git ls-files -u | wc -l);
+    echo "Conflicts detected with $branch : $NUMBER_OF_FILES files affected";
+    echo "Contact $AUTHOR to discuss whether you should continue your branch or not";
   fi
-  git reset --hard $branch;
+  git reset --hard $branch &> /dev/null;
 done
 
 git checkout -f $CURRENT_BRANCH &> /dev/null;
 git stash pop &> /dev/null;
-
-set +e
